d1d8d5c1326e0a794b72bd79c8b4d880c4f3a4a8
Deepen Timalsina <deepentimalsina05@gmail.com>
2024-11-18 20:23:58 +0545

    fix: optional chaining added in map and syntax corrected for useState hook

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,3 +1,4 @@
+
 import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../context/ShopContext"
 import Title from "./Title";
@@ -5,11 +6,12 @@ import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
     const {products}= useContext(ShopContext);
-    const {latestProducts, setLatestProducts}= useState([]);
+    const [latestProducts, setLatestProducts]= useState([]);
 
     useEffect(()=>{
         setLatestProducts(products.slice(0,10));
     },[])
+
   return (
     <div className="my-10">
         <div className="text-center py-8 text-3xl">
@@ -20,7 +22,7 @@ const LatestCollection = () => {
     {/* Redering products */}
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4gap-y-6">
         {
-            latestProducts.map((item,index)=>(
+            latestProducts?.map((item,index)=>(
                 <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
             ))
         }
