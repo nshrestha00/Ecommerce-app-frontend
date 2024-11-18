76d84bd11597573e400e33e889a6d275a317964e
Your Name <your.email@example.com>
2024-11-18 20:42:45 +0545

    added Link tag to logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium tw-border tw-border-slate-200">
-        <img src={assets.logo} alt="" className="w-32" />
+        <Link to="/"><img src={assets.logo} alt="" className="w-32" /></Link>
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to="/" className='flex flex-col items-center gap-1'>
