406414883d7a5bb09a5c64adbe92906d623080c1
Your Name <your.email@example.com>
2024-11-19 13:25:03 +0545

    Footer.jsx file created

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -25,8 +26,8 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/place-order' element={<PlaceOrder/>}/>
         <Route path='/orders' element={<Orders/>}/>
-
       </Routes>
+      <Footer/>
 
     </div>
   )
