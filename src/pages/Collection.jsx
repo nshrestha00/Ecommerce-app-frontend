214e92cbcd26e0ea69bb46bda8130b0ed226ac92
Your Name <your.email@example.com>
2024-11-20 12:17:19 +0545

    Added search functionalities

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const {products}=useContext(ShopContext);
+  const {products,search, showSearch}=useContext(ShopContext);
   const [showFilter, setShowFilter]= useState(false);
   const[filterProducts, setFilterProducts]= useState([]);
   const [category,setCategory]= useState([]);
@@ -31,6 +31,11 @@ const Collection = () => {
   
   const applyFilter=()=>{
     let productsCopy=products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()));
+      
+    }
 
     if(category.length>0){
       productsCopy =productsCopy.filter(item=>category.includes(item.category));
@@ -62,7 +67,7 @@ const sortProduct=()=>{
 
 useEffect(()=>{
   applyFilter();
-},[category, subCategory])
+},[category, subCategory,search,showSearch])
 
 useEffect(()=>{
   sortProduct();
