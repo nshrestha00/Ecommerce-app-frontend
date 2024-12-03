1bf6b9de7aabbb65b9b45505597e7768671fd97d
Your Name <your.email@example.com>
2024-12-03 13:53:59 +0545

    Added: Register and Login Api

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -14,6 +14,7 @@ const ShopContextProvider= (props)=>{
     const [showSearch, setShowSearch]= useState(false);
     const [cartItems,setCartItems]=useState({});
     const [products, setProducts]= useState([]);
+    const [token,setToken]=useState('');
     const navigate=useNavigate();
 
     const addToCart= async (itemId, size)=>{
@@ -102,6 +103,12 @@ const ShopContextProvider= (props)=>{
     useEffect(()=>{
         getProductData();
     },[])
+
+    useEffect(()=>{
+        if (!token && localStorage.getItem('token')) {
+            setToken(localStorage.getItem('token'));   
+        }
+    },[])
     
     const value={
         products,
@@ -112,7 +119,8 @@ const ShopContextProvider= (props)=>{
         updateQuantity,
         getCartAmount,
         navigate,
-        backendUrl
+        backendUrl,
+        setToken,token
     }
 
     return(
