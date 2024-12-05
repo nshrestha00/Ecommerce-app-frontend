ed23a8b46606d2fbe2b61efc0374c6ff38a9842e
Your Name <your.email@example.com>
2024-12-05 17:51:42 +0545

    integrated cart api

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -40,6 +40,16 @@ const ShopContextProvider= (props)=>{
             cartData[itemId][size]=1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(`${backendUrl}/api/cart/add`,{itemId,size},{headers:{token}});
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+            
+        }
     }
 
     const getCartCount= ()=>{
@@ -65,6 +75,16 @@ const ShopContextProvider= (props)=>{
         cartData[itemId][size]=quantity;
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(`${backendUrl}/api/cart/update`,{itemId,size,quantity},{headers:{token}});
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+            
+        }
     }
 
     const getCartAmount=() =>{
@@ -104,13 +124,29 @@ const ShopContextProvider= (props)=>{
         }
     }
 
+    const getUserCart= async (token)=>{
+        try {
+            const response = await axios.post(`${backendUrl}/api/cart/get`,{},{
+                headers: { token },
+            })
+
+            if (response.data.success) {
+                setCartItems(response.data.cartData);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     useEffect(()=>{
         getProductData();
     },[])
 
     useEffect(()=>{
         if (!token && localStorage.getItem('token')) {
-            setToken(localStorage.getItem('token'));   
+            setToken(localStorage.getItem('token')); 
+            getUserCart(localStorage.getItem('token'))  
         }
     },[])
     
