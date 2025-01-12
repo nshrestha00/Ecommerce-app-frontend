e56035cb2b5a6c7c01344f90f3a0bbc2e4b7c405
Your Name <your.email@example.com>
2025-01-12 14:16:53 +0545

    Update backend Url

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ export const ShopContext= createContext();
 const ShopContextProvider= (props)=>{
     const currency ='$';
     const delivery_fee=10;
-    const backendUrl=import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:4000'
+    const backendUrl='https://ecommerce-app-backend-uymb.onrender.com'
     const [search, setSearch]= useState('');
     const [showSearch, setShowSearch]= useState(false);
     const [cartItems,setCartItems]=useState({});
