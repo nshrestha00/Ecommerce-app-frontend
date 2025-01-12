7131faf36aae850a1cd9ebdc7dad7d8bf85540ca
Your Name <your.email@example.com>
2025-01-12 15:04:32 +0545

    Made carousel responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,23 @@ import "react-alice-carousel/lib/alice-carousel.css";
 
 const Hero = () => {
   const slides = [
-    <div key="1" className="flex justify-center items-center h-[400px]">
+    <div key="1" className="flex justify-center items-center">
       <img 
-        className="w-[80%] h-[400px] object-cover object-center rounded-lg" 
+        className="w-[80%] h-auto object-cover object-center rounded-lg" 
         src={assets.slide_one} 
         alt="" 
       />
     </div>,
-    <div key="2" className="flex justify-center items-center h-[400px]">
+    <div key="2" className="flex justify-center items-center">
       <img 
-        className="w-[80%] h-[400px] object-cover object-center rounded-lg" 
+        className="w-[80%] h-auto object-cover object-center rounded-lg" 
         src={assets.slide_two} 
         alt="" 
       />
     </div>,
-    <div key="4" className="flex justify-center items-center h-[400px]">
+    <div key="4" className="flex justify-center items-center">
       <img 
-        className="w-[80%] h-[400px] object-cover object-center rounded-lg" 
+        className="w-[80%] h-auto object-cover object-center rounded-lg" 
         src={assets.slide_four} 
         alt="" 
       />
@@ -28,8 +28,8 @@ const Hero = () => {
   ];
 
   return (
-    <div className="w-full h-[400px] flex items-center justify-center mt-4">
-      <div className="w-full max-w-6xl px-4">
+    <div className="w-full flex items-center justify-center mt-4">
+      <div className="w-full max-w-6xl px-4 sm:px-2">
         <AliceCarousel
           autoPlay
           autoPlayInterval={1000}
@@ -37,8 +37,8 @@ const Hero = () => {
           disableButtonsControls
           items={slides}
           responsive={{
-            0: { items: 1 },
-            768: { items: 1 },
+            0: { items: 1 }, 
+            1024: { items: 1 },
           }}
         />
       </div>
